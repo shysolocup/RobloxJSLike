0c34dd96ee79d9ecc24ec9b734e0aedf781dc4cd
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-12-03 23:54:07 -0600

    Update Errors.lua

--- JSLike/Config/Errors.lua
+++ JSLike/Config/Errors.lua
@@ -2,16 +2,22 @@ local JSLikeError = {};
 
 
 JSLikeError.Errors = {
-	["Object.Specify"] = "Invalid property descriptor. Cannot have a get/set method and a value in the same descriptor.",
-	["Object.NoSet"] = "Cannot set property %s of object which only has a get method and no set method.",
-	["Object.ReadOnly"] = "Cannot set %s of read-only object.",
+	["Object.Specify"] = "Invalid property descriptor. Cannot have a get/set method and a value in the same descriptor",
+	["Object.NoSet"] = "Cannot set property %s of object which only has a get method and no set method",
+	["Object.ReadOnly"] = "Cannot set %s of read-only object",
 	["Object.NonExt"] = "Cannot extend %s from %s because %s is not extendable."
 }
 
 
 
 function JSLikeError.throw(err, ...)
-	error(string.format("JSLikeError: "..JSLikeError.Errors[err], ...));
+	local err = "JSLikeError: "..JSLikeError.Errors[err];
+	
+	if #{...} > 0 then
+		error(string.format(err, ...));
+	else
+		error(err);
+	end
 end
 
 
